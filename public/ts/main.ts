45783b1c8f923babece563995fa21b5a70db4648
BHI <il0616@knu.ac.kr>
2016-08-03 17:55:02 +0900

    TypeScript 학습 중

---
+++ public/ts/main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by manager on 2016-08-03.
+ */
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { AppComponent } from './ts-test';
+
+bootstrap(AppComponent);
